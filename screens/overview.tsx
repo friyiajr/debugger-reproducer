1aebc7059034e67ef700f66f0c37a3a8d2844ff6
Daniel Friyia <dfriyia@gmail.com>
2024-11-15 21:55:57 -0500

    Initial Commit

--- screens/overview.tsx
+++ screens/overview.tsx
@@ -1,27 +1,15 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { ScreenContent } from 'components/ScreenContent';
 import { StyleSheet, View } from 'react-native';
 
 import { Button } from '../components/Button';
-import { RootStackParamList } from '../navigation';
-
-type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Overview'>;
 
 export default function Overview() {
-  const navigation = useNavigation<OverviewScreenNavigationProps>();
+  const onPress = () => {
+    console.log('DEBUGGER HIT');
+  };
 
   return (
     <View style={styles.container}>
-      <ScreenContent path="screens/overview.tsx" title="Overview" />
-      <Button
-        onPress={() =>
-          navigation.navigate('Details', {
-            name: 'Dan',
-          })
-        }
-        title="Show Details"
-      />
+      <Button onPress={onPress} title="Debugger" />
     </View>
   );
 }
